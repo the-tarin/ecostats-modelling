6c0211a721056b81f8cdb0b8f85000de6c314115
Tarin Eccleston <tarin.eccleston@gmail.com>
2023-09-08 16:59:15 +1200

    generate distance matrix between mic and gibbon groups

--- data-generators/hazard_model_data_generator.R
+++ data-generators/hazard_model_data_generator.R
@@ -7,6 +7,7 @@ y_mic = seq(from = 2000, to = 8000, by = 2000)
 
 mic_coords = expand.grid(x = x_mic, y = y_mic)
 colnames(mic_coords) = c('x_mic', 'y_mic')
+mic_coords = as.matrix(mic_coords)
 
 # expectation of gibbon groups in survey area
 intensity = 0.000001
@@ -20,12 +21,24 @@ x_gibbon_group <- round(runif(expected_gibbon_groups, 0, x_perimeter))
 y_gibbon_group <- round(runif(expected_gibbon_groups, 0, y_perimeter))
 
 gibbon_group_coords = cbind(x_gibbon_group, y_gibbon_group)
+gibbon_group_coords = as.matrix(gibbon_group_coords)
 
 # TODO: this plot looks like ass, please fix
 plot(x_gibbon_group, y_gibbon_group)
 
+# euclidean distance for detector to gibbon observation
+dist_mic_gibbon <- matrix(NA, nrow = nrow(mic_coords), ncol = nrow(gibbon_group_coords))
+
+for (i in 1:nrow(dist_mic_gibbon)) {
+  for (j in 1:ncol(dist_mic_gibbon)) {
+    dist_mic_gibbon[i, j] <- sqrt(sum((mic_coords[i, ] - gibbon_group_coords[j, ])^2))
+  }
+}
+
+# half normal hazard distribution
 lambda = 500
 sigma = 50
+
 
 
 
