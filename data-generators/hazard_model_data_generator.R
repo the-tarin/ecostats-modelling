79f0f37cc67567fa9baf09a280ccdcc41b9692ea
Tarin Eccleston <tarin.eccleston@gmail.com>
2023-09-08 14:15:39 +1200

    generate plot of mics

---
+++ data-generators/hazard_model_data_generator.R
@@ -0,0 +1,5 @@
+# x and y in square survey site with dimensions 10kmx10km with markers every 2000m
+x <- seq(from = 0, to = 10000, by = 2000)
+y <- seq(from = 0, to = 10000, by = 2000)
+
+expand.grid(x = x, y = y)
