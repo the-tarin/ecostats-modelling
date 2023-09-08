38e30bf0c59b023f8dcd8ff04734ddc7fc6cbadc
Tarin Eccleston <tarin.eccleston@gmail.com>
2023-09-08 16:27:22 +1200

    homogeneous Poisson point process for gibbon coordinates

--- data-generators/hazard_model_data_generator.R
+++ data-generators/hazard_model_data_generator.R
@@ -1,5 +1,21 @@
-# x and y in square survey site with dimensions 10kmx10km with markers every 2000m
-x <- seq(from = 0, to = 10000, by = 2000)
-y <- seq(from = 0, to = 10000, by = 2000)
+set.seed(123)
 
-expand.grid(x = x, y = y)
+# x and y in square survey site with dimensions 10kmx10km with markers every 2000m
+# mics
+x_mic <- seq(from = 2000, to = 8000, by = 2000)
+y_mic <- seq(from = 2000, to = 8000, by = 2000)
+
+mic_coords = expand.grid(x = x_mic, y = y_mic)
+
+# TODO: add micID to mic_coords df
+
+# 
+intensity = 0.000001
+x_perimeter = 10000
+y_perimeter = 10000
+survey_area = x_perimeter * y_perimeter
+expected_gibbon_groups = intensity * survey_area
+
+# homogeneous Poisson point process for getting gibbon coordinates
+x_gibbon_group <- runif(expected_gibbon_groups, 0, x_perimeter)
+y_gibbon_group <- runif(expected_gibbon_groups, 0, y_perimeter)
