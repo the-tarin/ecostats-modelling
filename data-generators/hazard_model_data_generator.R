0f9ca237ffd1bc2c9cf7cbc8d310ba4543b015c2
Tarin Eccleston <tarin.eccleston@gmail.com>
2023-09-15 11:02:49 +1200

    plot detected gibbon group

--- data-generators/hazard_model_data_generator.R
+++ data-generators/hazard_model_data_generator.R
@@ -1,6 +1,6 @@
 set.seed(123)
 
-# x and y in square survey site with dimensions 10kmx10km with markers every 2000m
+# x and y in square survey site with dimensions 10km x 10km with markers every 2000m
 # mics
 x_mic = seq(from = 2000, to = 8000, by = 2000)
 y_mic = seq(from = 2000, to = 8000, by = 2000)
@@ -23,6 +23,7 @@ y_gibbon_group <- round(runif(expected_gibbon_groups, 0, y_perimeter))
 gibbon_group_coords = cbind(x_gibbon_group, y_gibbon_group)
 gibbon_group_coords = as.matrix(gibbon_group_coords)
 
+# plot of mic grid and Poisson point process distributed gibbon groups
 {
 plot(x_gibbon_group, y_gibbon_group, type = "p", col = "blue", pch = 1, xlab = "X-coordinate (m)", ylab = "Y-coordinate (m)")
 points(mic_coords[,1], mic_coords[,2], type = "p", col = "red", pch = 15)
@@ -37,7 +38,7 @@ for (i in 1:nrow(dist_mic_gibbon)) {
   }
 }
 
-# half normal hazard distribution for probablity matrix
+# half normal hazard distribution for probability matrix
 lambda = 50
 sigma = 500
 
@@ -56,3 +57,15 @@ for (i in 1:nrow(dist_mic_gibbon)) {
     detection_matrix[i, j] <- rbinom(size = 1, n = 1, prob = detection_prob_matrix[i,j])
   }
 }
+
+# plot detected gibbon groups for chosen mic
+plot_select = 9
+
+for (i in plot_select) {
+  detection_gibbon_group_idx = which(detection_matrix[i,] == 1)
+  print(detection_gibbon_group_idx)
+  plot(x_gibbon_group, y_gibbon_group, type = "p", col = "blue", pch = 1, xlab = "X-coordinate (m)", ylab = "Y-coordinate (m)")
+  points(mic_coords[,1], mic_coords[,2], type = "p", col = "red", pch = 15)
+  points(x_gibbon_group[detection_gibbon_group_idx], y_gibbon_group[detection_gibbon_group_idx], type = "p", col = "green", pch = 15)
+}
+
